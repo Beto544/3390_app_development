b8767e7f261669686157bb05831de99fff83954e
SalvatoreAguilera <143650500+SalvatoreAguilera@users.noreply.github.com>
2023-10-05 19:39:42 -0700

    Fixing backend
    Added changes to check if the user already exists

--- BACKEND/controllers/user.js
+++ BACKEND/controllers/user.js
@@ -1,9 +1,13 @@
 const User = require('../model/user')
+const jwt = require('jsonwebtoken')
+
 
 exports.createUser = async (req,res) => {
     const {name, email, password} = req.body
-    const user = await User.find({email})
-    if(user) res.status(400).json({sucess:false, error:' email already exists'})
+    const user = await User.findOne({email})
+    if(user) return res
+        .status(400)
+        .json({sucess:false, error:' email already exists'})
     const newUser = new User({
         name ,
         email ,
@@ -11,4 +15,27 @@ exports.createUser = async (req,res) => {
     })
     await newUser.save();
     res.send(newUser)
+}
+
+exports.signin = async (req,res) => {
+    const {email,password} = req.body
+    if(!email.trim() || !password.trim) return res
+    .status(400)
+    .json({sucess:false, error:'email/password missing'});
+    
+    const user = await User.findOne({email})
+    if(!user) return res
+    .status(400)
+    .json({sucess:false, error:'User not found'});
+
+    const isMatched = await user.comparePassword(password)
+    if(!isMatched) return res
+    .status(400)
+    .json({sucess:false, error:'email/password does not match'});
+
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
+        expiresIn: '1d'
+    })
+    res.json({success: true, user: {name: user.name, email: user.email, id: user._id, token}})
+
 }
--- BACKEND/routes/user.js
+++ BACKEND/routes/user.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 
 const {createUser} = require("../controllers/user");
+const {signin} = require("../controllers/user");
 const { validateUser, validate } = require('../middlewares/validator');
 
 
 router.post('/create', validateUser, validate, createUser)
+router.post('/signin', signin)
 
 
 
