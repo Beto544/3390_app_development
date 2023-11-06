41f93d4ee891a142d247ede56162276bc5f83113
Oscar <90999876+Shad0wMag1c@users.noreply.github.com>
2023-11-06 09:02:47 -0800

    flip command and notecard design

--- screens/CreateFlashCards.js
+++ screens/CreateFlashCards.js
@@ -1,52 +1,69 @@
 import React, {useState} from "react";
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet, Button, TextInput } from "react-native";
-import { printToFileAsync } from 'expo-print';
-import { shareAsync } from 'expo-sharing';
-import { printAsync } from 'expo-print';
-import * as FileSystem from 'expo-file-system';
+import { View, Text, StyleSheet, Button, TextInput, Animated } from "react-native";
 
 export default function CreateFlashCards(){
-    let [name, setName] = useState("");
     const [question, setQuestion] = useState("");
     const [answer, setAnswer] = useState("");
+    const [flashcards, setFlashcards] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [flip, setFlip] = useState(false);
 
-        const html = `
-        <html>
-        <body>
-            <h1>Flashcard</h1>
-            <p>Question: ${question}</p>
-            <p>Answer: ${answer}</p>
-                </body>
-            </html>
-            `;
-            //let GeneratePdf = async() => {
-            //    const file = await printToFileAsync({
-            //        html: html,
-            //        base64: false
-            //    });
-            //    await shareAsync(file.uri);
-            //};
-            let GeneratePdf = async () => {
-                const file = await printAsync({
-                  html: html,
-                });
-                await shareAsync(file.uri);
-              };
+    const addFlashcard = () => {
+        if (question.trim() === "" || answer.trim() === "") {
+            return;
+        }
+        setFlashcards([...flashcards, {question, answer}]);
+        setQuestion("");
+        setAnswer("");
+    };
+
+    const flipFlashcard = () => {
+        setFlip(!flip);
+    };
+
+    const nextFlashcard = () => {
+        if (currentIndex < flashcards.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        }
+    };
+    const previousFlashcard = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    };
         return (
             <View style={styles.container}>
-                <TextInput value={question} 
-                placeholder="Type question here" 
-                style={styles.textInput} 
+
+                <TextInput
+                placeholder="Type question here"
+                value={question} 
+                style={[styles.textInput, {height: 100}]}
                 onChangeText={(value) => setQuestion(value)}
                 multiline={true}
-                numberOfLines={4} />
-                <TextInput value={answer} 
+                />
+
+                <TextInput 
                 placeholder="Type answer here" 
-                style={styles.textInput} 
-                onChangeText={(value) => setAnswer(value)} />
-                <Button title="Generate PDF" onPress={GeneratePdf}/>
-                <StatusBar style="auto" />
+                value={answer}
+                style={[styles.textInput, {height: 100}]} 
+                onChangeText={(value) => setAnswer(value)} 
+                multiline={true}
+                />
+
+                <Button title="Add Flashcard" onPress={addFlashcard} />
+                {flashcards.length > 0 &&(
+                    <View>
+                        <Text>{flip ? "Answer" : "Question"}</Text>
+                        <Text style = {styles.flashcardText}>
+                            {flip ?
+                            flashcards[currentIndex].answer :
+                            flashcards[currentIndex].question}
+                        </Text>
+                        <Button title="Flip" onPress={flipFlashcard} color="#007AFF" />
+                        <Button title="Next" onPress={nextFlashcard} color="#007AFF" />
+                        <Button title="Previous" onPress={previousFlashcard} color="#007AFF" />
+                    </View>
+                )}
             </View>
     );
   }
@@ -56,14 +73,32 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center", // Center vertically
         alignItems: "center",     // Center horizontally
+        padding: 20,
+        backgroundColor: "#f5f5f5",
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     },
     textInput: {
-        alignSelf: "stretch",
+        marginBottom: 10,
         padding: 8,
-        margin: 8,
         borderWidth: 1,
         borderColor: "#ccc",
-        borderRadius: 5,
-        fontSize: 16,
-    }
+        width: "100%"
+    },
+    flashcardText: {
+        fontSize: 18,
+        textAlign: "center",
+        marginVertical: 10,
+        padding: 20,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 10,
+      },
 });
